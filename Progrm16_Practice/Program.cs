0025fb948ede192b980efca429cabe0b21ee908e
YS AN <as5948@naver.com>
2023-05-07 23:04:07 +0900

    feat : Modify Program

--- Progrm16_Practice/Program.cs
+++ Progrm16_Practice/Program.cs
@@ -4,7 +4,29 @@
 	{
 		static void Main(string[] args)
 		{
-			
+			/*FindStringIndex findStringIndex = new FindStringIndex();
+			findStringIndex.DoFindStringIndex();
+
+			CountString countString = new CountString();
+			countString.DoCountString();
+
+			CheckedPrimeNumber checkedPrimeNumber = new CheckedPrimeNumber();
+			checkedPrimeNumber.DoCheckedPrimeNumber();
+
+			DigitalSum digitalSum = new DigitalSum();
+			digitalSum.DoDigitalSum();
+
+			FindCommonNumber findCommonNumber = new FindCommonNumber();
+			findCommonNumber.DoFindCommonNumber();
+
+			UpAndDown upAndDown = new UpAndDown();
+			upAndDown.DoPlay();
+
+			SlidingPuzzle slidingPuzzle = new SlidingPuzzle();
+			slidingPuzzle.DoPlay(); //*/
+
+			Bingo bingo = new Bingo();
+			bingo.DoPlay();
 		}
 	}
 }
